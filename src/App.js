27a50649e0db6f9375119f91f38adfb385ff9d2d
Uday <uday@swapmotors.com>
2020-09-20 16:53:52 +0530

    Completed project + firebase deploy.

--- src/App.js
+++ src/App.js
@@ -1,111 +1,138 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
-import InfoBox from './InfoBox';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 import {
+  MenuItem,
   FormControl,
   Select,
-  MenuItem,
   Card,
-  CardContent
+  CardContent,
 } from "@material-ui/core";
+import InfoBox from "./InfoBox";
+import LineGraph from "./LineGraph";
+import Table from "./Table";
+import { sortData, prettyPrintStat } from "./util";
+import numeral from "numeral";
+import Map from "./Map";
+import "leaflet/dist/leaflet.css";
 
-import Map from './Map';
-import Table from './Table';
-import { sortData } from './util';
-import LineGraph from './LineGraph';
+const App = () => {
+  const [country, setInputCountry] = useState("worldwide");
+  const [countryInfo, setCountryInfo] = useState({});
+  const [countries, setCountries] = useState([]);
+  const [mapCountries, setMapCountries] = useState([]);
+  const [tableData, setTableData] = useState([]);
+  const [casesType, setCasesType] = useState("cases");
+  const [mapCenter, setMapCenter] = useState({ lat: 34.80746, lng: -40.4796 });
+  const [mapZoom, setMapZoom] = useState(3);
 
-const GET_COUNTRIES_API = "https://disease.sh/v3/covid-19/countries";
-const GET_ALL_API = "https://disease.sh/v3/covid-19/all";
-const GET_SPECIFIC_COUNTRY = "https://disease.sh/v3/covid-19/countries/";
-
-function App() {
-  const [countries, setCountries] = useState([])
-  const [country, setCountry] = useState('worldwide')
-  const [countryInfo, setCountryInfo] = useState({})
-  const [tableData, setTableData] = useState([])
-  
-
-  // fetch countries using useEffec
   useEffect(() => {
+    fetch("https://disease.sh/v3/covid-19/all")
+      .then((response) => response.json())
+      .then((data) => {
+        setCountryInfo(data);
+      });
+  }, []);
 
+  useEffect(() => {
     const getCountriesData = async () => {
-      await fetch(GET_COUNTRIES_API)
+      fetch("https://disease.sh/v3/covid-19/countries")
         .then((response) => response.json())
         .then((data) => {
-          const countries = data.map(country => ({
+          const countries = data.map((country) => ({
             name: country.country,
-            value: country.countryInfo.iso2
-          }))
-
-          const tableData = data.map(country => ({
-            country: country.country,
-            cases: country.cases
-          }))
-          setTableData(sortData(tableData));
+            value: country.countryInfo.iso2,
+          }));
+          let sortedData = sortData(data);
           setCountries(countries);
-        })
-    }
+          setMapCountries(data);
+          setTableData(sortedData);
+        });
+    };
 
     getCountriesData();
-  }, [])
+  }, []);
 
-  useEffect(() => {
-    fetch(GET_ALL_API)
-      .then(response => response.json())
-      .then(data => {
+  console.log(casesType);
+
+  const onCountryChange = async (e) => {
+    const countryCode = e.target.value;
+
+    const url =
+      countryCode === "worldwide"
+        ? "https://disease.sh/v3/covid-19/all"
+        : `https://disease.sh/v3/covid-19/countries/${countryCode}`;
+    await fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setInputCountry(countryCode);
         setCountryInfo(data);
-      })
-  }, [])
-
-  useEffect(() => {
-    const url = country === 'worldwide' ? GET_ALL_API : GET_SPECIFIC_COUNTRY + `${country}`;
-    fetch(url)
-      .then(response => response.json())
-      .then(data => {
-        setCountryInfo(data);
-      })
-  }, [country])
-
-  const onCountryChange = async (event) => {
-    const country = event.target.value;
-    setCountry(country);
-  }
+        setMapCenter([data.countryInfo.lat, data.countryInfo.long]);
+        setMapZoom(4);
+      });
+  };
 
   return (
     <div className="app">
-      <div className="app_left">
-        <div className="app_header">
-          <h1> COVID19 TRACKER</h1>
-          <FormControl className="app_dropdown">
-            <Select variant="outlined" value={country} onChange={onCountryChange}>
-              <MenuItem value='worldwide'>Worldwide</MenuItem>
-              {
-                countries.map((country) => (
-                  <MenuItem value={country.value}>{country.name}</MenuItem>
-                ))
-              }
+      <div className="app__left">
+        <div className="app__header">
+          <h1>COVID-19 Tracker</h1>
+          <FormControl className="app__dropdown">
+            <Select
+              variant="outlined"
+              value={country}
+              onChange={onCountryChange}
+            >
+              <MenuItem value="worldwide">Worldwide</MenuItem>
+              {countries.map((country) => (
+                <MenuItem value={country.value}>{country.name}</MenuItem>
+              ))}
             </Select>
           </FormControl>
         </div>
-
-        <div className="app_stats">
-          <InfoBox title='Coronavirus Cases' cases={countryInfo.todayCases} total={countryInfo.cases}></InfoBox>
-          <InfoBox title='Recoveries' cases={countryInfo.todayRecovered} total={countryInfo.recovered}></InfoBox>
-          <InfoBox title='Death Cases' cases={country.todayDeaths} total={countryInfo.deaths}></InfoBox>
+        <div className="app__stats">
+          <InfoBox
+            onClick={(e) => setCasesType("cases")}
+            title="Coronavirus Cases"
+            isRed
+            active={casesType === "cases"}
+            cases={prettyPrintStat(countryInfo.todayCases)}
+            total={numeral(countryInfo.cases).format("0.0a")}
+          />
+          <InfoBox
+            onClick={(e) => setCasesType("recovered")}
+            title="Recovered"
+            active={casesType === "recovered"}
+            cases={prettyPrintStat(countryInfo.todayRecovered)}
+            total={numeral(countryInfo.recovered).format("0.0a")}
+          />
+          <InfoBox
+            onClick={(e) => setCasesType("deaths")}
+            title="Deaths"
+            isRed
+            active={casesType === "deaths"}
+            cases={prettyPrintStat(countryInfo.todayDeaths)}
+            total={numeral(countryInfo.deaths).format("0.0a")}
+          />
         </div>
-        <Map />
+        <Map
+          countries={mapCountries}
+          casesType={casesType}
+          center={mapCenter}
+          zoom={mapZoom}
+        />
       </div>
-      <Card className="app_right">
+      <Card className="app__right">
         <CardContent>
-          <h3>Cases by country</h3>
-          <Table countries={tableData}></Table>
-          <h4>Graphs</h4>
-          <LineGraph/>
+          <div className="app__information">
+            <h3>Live Cases by Country</h3>
+            <Table countries={tableData} />
+            <h3 className="app__graph__title">Worldwide new {casesType}</h3>
+            <LineGraph className="app__graph" casesType={casesType} />
+          </div>
         </CardContent>
       </Card>
-
     </div>
   );
-}
+};
 
 export default App;
--- src/Table.js
+++ src/Table.js
@@ -1,18 +1,20 @@
-import React from 'react'
-import './Table.css';
+import React from "react";
+import "./Table.css";
+import numeral from "numeral";
 
 function Table({ countries }) {
-    return <div className="table">
-        {
-            countries.map(({ country, cases }) => (
-                
-                    <tr>
-                        <td>{country}</td>
-                        <strong><td>{cases}</td></strong>
-                    </tr>
-            ))
-        }
-    </div>
+    return (
+        <div className="table">
+            {countries.map((country) => (
+                <tr>
+                    <td>{country.country}</td>
+                    <td>
+                        <strong>{numeral(country.cases).format("0,0")}</strong>
+                    </td>
+                </tr>
+            ))}
+        </div>
+    );
 }
 
-export default Table
+export default Table;
